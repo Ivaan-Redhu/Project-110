2861959415537795e53749344a3812df530c6eaa
Ivaan-Redhu <93902463+Ivaan-Redhu@users.noreply.github.com>
2023-03-12 17:20:19 +1300

    Add files via upload

--- main.js
+++ main.js
@@ -19,4 +19,67 @@ function take_snapshot() {
 
 console.log('ml5 version:', ml5.version);
 
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/aNqWOUiPX/model.json', modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/aNqWOUiPX//model.json', modelLoaded);
+
+function check() {
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function speak() {
+    var synth = window.speechSynthesis;
+    speak_data_1 = "The first prediction is " + prediction_1;
+    speak_data_2 = "And the second prediction is " + prediction_2;
+    var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
+    synth.speak(utterThis);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);;
+        document.getElementById("result_gesture_name").innerHTML = results[0].label;
+        prediction_1 = results[0].label;
+
+        speak();
+        if (results[0].label == "perfect") {
+            document.getElementById("update_gesture").innerHTML = "&#128076;";
+        }
+        if (results[0].label == "thumbs up") {
+            document.getElementById("update_gesture").innerHTML = "&#128077;";
+        }
+        if (results[0].label == "peace") {
+            document.getElementById("update_gesture").innerHTML = "&#9996;";
+        }
+        if (results[0].label == "fist") {
+            document.getElementById("update_gesture").innerHTML = "&#9994;";
+        }
+        if (results[0].label == "clap") {
+            document.getElementById("update_gesture").innerHTML = "&#128080;";
+        }
+        if (results[0].label == "punk") {
+            document.getElementById("update_gesture").innerHTML = "&#129304;";
+        }
+
+
+        if (results[1].label == "perfect") {
+            document.getElementById("update_emoji2").innerHTML = "&#128076;";
+        }
+        if (results[1].label == "thumbs up") {
+            document.getElementById("update_emoji2").innerHTML = "&#128077;";
+        }
+        if (results[1].label == "peace") {
+            document.getElementById("update_emoji2").innerHTML = "&#9996;";
+        }
+        if (results[0].label == "fist") {
+            document.getElementById("update_gesture").innerHTML = "&#9994;";
+        }
+        if (results[0].label == "clap") {
+            document.getElementById("update_gesture").innerHTML = "&#128080;";
+        }
+        if (results[0].label == "punk") {
+            document.getElementById("update_gesture").innerHTML = "&#129304;";
+        }
+    }
+}
